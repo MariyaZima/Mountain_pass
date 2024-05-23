315bbc8054c3d1d7c6b971faef0aa6807e22568e
Mariya <79620777542@ya.ru>
2024-05-23 19:56:01 +0300

    Создан файл yasg.py для генерации документации с помощью Swagger

--- Mountain_pass/Mountain_pass/settings.py
+++ Mountain_pass/Mountain_pass/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'MPass',
     'rest_framework',
     'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+#SWAGGER_SETTINGS = {"DEFAULT_GENERATOR_CLASS": "rest_framework.schemas.generators.BaseSchemaGenerator", }
--- Mountain_pass/Mountain_pass/urls.py
+++ Mountain_pass/Mountain_pass/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from MPass import views
+from .yasg import urlpatterns as doc_urls
 
 router = routers.DefaultRouter()
 router.register(r'Mountains', views.MountainViewset)
@@ -31,3 +32,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns += doc_urls
